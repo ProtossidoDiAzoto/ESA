0b790b1ce65aa2f5e037ea2f6ff4aba4e2d21b60
ProtossidoDiAzoto <lorenzo.matteucci@hotmail.it>
2018-10-31 18:32:06 +0100

    Update RC.ino

--- Arduino/Lezione Laboratorio 1/RC/RC.ino
+++ Arduino/Lezione Laboratorio 1/RC/RC.ino
@@ -1,30 +1,62 @@
 #include "RC.h"
 
+String command = "full\n";
+bool displayedError = false;
+
 void setup(){
 	pinMode(READ_PIN,INPUT);
   Serial.begin(BAUDRATE);
 }
 
 void loop(){
-  /*Avvio ciclo*/
+   
   short counter = 0;
   float values = 0;
-  /*Campionamento per valori*/
+    
   while(counter < WINDOW_MAX_SIZE){
     values = values + analogRead(READ_PIN);
     counter++;
   }
-  /*Termino il ciclo*/
+    
   long timer = millis();
   int timerSec = timer/1000;
   float vCapacitor = Vc(VS, timerSec, TAU, CAPACITOR_CAPACITY);
-  Serial.print(filterValue(values, counter));
-  Serial.print(",");
-  Serial.print(timer);
-  Serial.print(",");
-  Serial.print(vCapacitor);
-  Serial.print(",");
-  Serial.println(currentIntensity(vCapacitor, VS, timerSec, TAU));
+   
+  if(Serial.available()){
+    command = Serial.readString();
+    }
+
+   //ALL MEASURES DETECTED
+   if(command =="full\n"){
+            Serial.print(filterValue(values, counter));
+            Serial.print(",");
+            Serial.print(timer);
+            Serial.print(",");
+            Serial.print(vCapacitor);
+            Serial.print(",");
+            Serial.println(currentIntensity(vCapacitor, VS, timerSec, TAU));
+            displayedError = false;
+      } else 
+      // CAPACITOR VOLTAGE and TIME COSTANT
+         if(command =="capv\n"){
+            Serial.print(filterValue(values, counter));
+            Serial.print(",");
+            Serial.println(timer);
+          displayedError = false;
+      } else 
+      // CAPACITOR CURRENT and TIME COSTANT
+         if(command =="capc\n"){
+            Serial.print("i capacitor");
+            Serial.print(",");
+            Serial.println(timer);
+          displayedError = false;
+      } else {
+          if(!displayedError){
+            Serial.println("WRONG COMMAND: the only correct commands are: \n full to obtain all measures detected \n capv to obtain capacitor's voltage \n capn to obtain capacitor's current");
+            displayedError = true;
+          }
+          
+      }
 }
 
 float filterValue(float values, int counter){
@@ -33,7 +65,8 @@ float filterValue(float values, int counter){
 
 float Vc(float vs, long t, float R, float C){
    float e = 2.72;
-   return vs * pow(e,(-1)*(t/(R*C)));
+   float exponent = pow(e,(-1)*(t/(R*C)));
+   return vs * (1- exponent);
 }
 
 float currentIntensity(float vc, float vs, long t, float r){
